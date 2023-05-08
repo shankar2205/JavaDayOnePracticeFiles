00e01823828af6d5a4363b2184b48d97878ddfba
“Shankar2205” <shankerb582@gmail.com>
2023-05-08 15:11:38 +0530

    [Shankar] : DefaultValues file added.

---
+++ Default.java
@@ -0,0 +1,19 @@
+public class DefaultValues {
+    static int integerValue;
+    static String stringValue;
+    static double doubleValue;
+    static float floatValue;
+    static long longValue;
+
+    public static void main(String[] args) {
+
+        System.out.println("Printing Default Values......!");
+
+        System.out.println("Default Integer Value is : " +integerValue);
+        System.out.println("Default String Value is : " +stringValue );
+        System.out.println("Default Double Value is : " +doubleValue);
+        System.out.println("Default Float Value is : " +floatValue);
+        System.out.printf("Default Long Value is : " +longValue);
+
+    }
+}
